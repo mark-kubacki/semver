255c85a1778703553b6993eb882dcb2974425211
Mark Kubacki <wmark@hurrikane.de>
2019-07-31 23:01:43 +0200

    CI: use Google's images for Go

--- go.mod
+++ go.mod
@@ -1,1 +1,1 @@
-module github.com/wmark/semver
+module blitznote.com/src/semver/v2
--- semver.go
+++ semver.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package semver contains types and functions for
-// parsing of Versions and (Version-)Ranges.
 package semver
 
 import (
